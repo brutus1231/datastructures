1b531bbf9e252fe1a7a77da23997c85ec6294c2d
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2018-10-20 13:04:26 +0200

    Initial commit

---
+++ src/main/java/FibonacciChain.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FibonacciChain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj rozmiar tablicy: ");
+        int n = scanner.nextInt();
+
+        long[] chain = fill(n);
+        print(chain);
+    }
+
+    private static void print(long[] chain) {
+        System.out.println(Arrays.toString(chain));
+    }
+
+    private static long[] fill(int n) {
+        long[] result = new long[n];
+
+        result[0] = 0;
+        result[1] = 1;
+        for (int i = 2; i < n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result;
+    }
+
+
+}
---
+++ src/main/java/RandomTable.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj rozmiar tablicy: ");
+        int n = scanner.nextInt();
+
+        int[] table = fill(n);
+        print(table);
+
+        System.out.println("Czy tablica jest niemajejąca " + checkOrder(table, true));
+        System.out.println("Czy tablica jest nierosnąca " + checkOrder(table, false));
+    }
+
+    private static boolean checkOrder(int[] table, boolean isGrater) {
+        boolean result = true;
+
+        for (int i = 1; i < table.length; i++) {
+            if (isGrater && table[i] < table[i - 1]) {
+                return false;
+            }
+            if (!isGrater && table[i] > table[i - 1]) {
+                return false;
+            }
+        }
+
+        return result;
+    }
+
+    private static void print(int[] table) {
+        System.out.println(Arrays.toString(table));
+    }
+
+    private static int[] fill(int n) {
+        int[] result = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            result[i] = random.nextInt(100);
+        }
+        return result;
+    }
+
+
+}
