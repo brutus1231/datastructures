838f2ad86dedc9b6c7635f890d0476ad10b68605
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2018-10-20 15:43:27 +0200

    priority queue comparator

--- src/main/java/pl/sda/CustomPriorityQueue.java
+++ src/main/java/pl/sda/CustomPriorityQueue.java
@@ -1,10 +1,19 @@
 package pl.sda;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class CustomPriorityQueue<T> {
     private Queue<T> elements = new PriorityQueue<T>();
+
+    public CustomPriorityQueue(){
+
+    }
+
+    public CustomPriorityQueue(Comparator comparator){
+        elements = new PriorityQueue<T>(comparator);
+    }
 
     public void push(T element) {
         elements.add(element);
@@ -34,16 +43,47 @@ public class CustomPriorityQueue<T> {
     }
 
     public static void main(String[] args) {
-        CustomPriorityQueue<Integer> queue1 = new CustomPriorityQueue<>();
-        queue1.push(1);
-        queue1.push(2);
-        queue1.push(3);
-        queue1.clear();
+//        CustomPriorityQueue<Integer> queue1 =
+//                new CustomPriorityQueue<>(getNumberComparator());
+//        queue1.push(1);
+//        queue1.push(2);
+//        queue1.push(3);
+//        queue1.clear();
+//
+//        CustomPriorityQueue<String> queue2 = new CustomPriorityQueue<>();
+//        queue2.push("ala ma kota");
+//        queue2.push("wiewiórka");
+//        queue2.push("programowanie");
+//        queue2.clear();
 
-        CustomPriorityQueue<String> queue2 = new CustomPriorityQueue<>();
-        queue2.push("ala ma kota");
-        queue2.push("wiewiórka");
-        queue2.push("programowanie");
-        queue2.clear();
+
+        String s1 = "zzzaz";
+        String s2 = "zzzaz";
+
+        System.out.println(s1.compareTo(s2));
+
+    }
+
+    private static Comparator<Integer> getNumberComparator() {
+        return new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+
+                if (o1 % 2 == o2 % 2) {
+                    return o1 < o2 ? -1 : 1;
+                }
+
+                if(o1 % 2 == 0){
+                    return -1;
+                }
+
+                if(o2 % 2 == 0){
+                    return 1;
+                }
+
+
+                return 0;
+            }
+        };
     }
 }
